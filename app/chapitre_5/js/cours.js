aade82a7bb1627c4dd08f412ca64946756c3abad
Nicooo <saoutn@gmail.com>
2016-08-23 11:03:25 +0200

    Chapitre_5 en cours - sans exos

---
+++ app/chapitre_5/js/cours.js
@@ -0,0 +1,8 @@
+function direBonjour(prenom) {
+  var msg = "Bonjour " + prenom + " !";
+  return msg;
+}
+
+console.log("Début du programme");
+console.log(direBonjour("Michel"));
+console.log("Fin du programme");
